c9cf4f68f7f916aeb177b5c9a88644c4bd16e305
潘帅 <panshuairg@hotmail.com>
2019-01-31 17:19:01 +0800

    add server

--- Source/RunGo.Repository/RunGo.Repository/Properties/AssemblyInfo.cs
+++ Source/RunGo.Repository/RunGo.Repository/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("RunGo.Repository")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("基于Dapper封装的数据库交互层")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("RongGuang.LTD")]
 [assembly: AssemblyProduct("RunGo.Repository")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyVersion("1.1.2.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
